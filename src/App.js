ea6394514b26a4dec4dc56062589a96b383b7d7b
Victor Bundi <victorbundi.dev@gmail.com>
2025-07-15 16:47:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+{/**Calculator
+
+Basic arithmetic.
+
+Use React state to update the display. */}
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
